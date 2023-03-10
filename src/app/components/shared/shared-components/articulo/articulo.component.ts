cba479b520d8d2affa2e7210096369571f2d96d8
Edgar Muñoz <edgmuo1085@gmail.com>
2023-03-10 09:01:40 -0500

    se agrega compartir

--- src/app/components/shared/shared-components/articulo/articulo.component.ts
+++ src/app/components/shared/shared-components/articulo/articulo.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { Article } from 'src/app/components/interfaces';
 import { Browser } from '@capacitor/browser';
+import { ActionSheetController } from '@ionic/angular';
+import { Share } from '@capacitor/share';
 
 @Component({
   selector: 'app-articulo',
@@ -11,11 +13,51 @@ export class ArticuloComponent {
   @Input() articulo: Article = {} as Article;
   @Input() i: number = 0;
 
-  onClick() {
-    console.log('onClick');
-  }
+  constructor(private actionSheetCtr: ActionSheetController) {}
 
   async urlRedirect() {
     await Browser.open({ url: this.articulo.url });
+  }
+
+  async openMenu() {
+    const actionSheet = await this.actionSheetCtr.create({
+      header: 'Opciones',
+      buttons: [
+        {
+          text: 'Compartir',
+          icon: 'share-outline',
+          handler: () => {
+            this.getCompartirArticulos();
+          },
+        },
+        {
+          text: 'Favorito',
+          icon: 'heart-outline',
+          handler: () => {
+            this.getToggleFavoritos();
+          },
+        },
+        {
+          text: 'Cancelar',
+          icon: 'close-outline',
+          role: 'cancel',
+        },
+      ],
+    });
+
+    await actionSheet.present();
+  }
+
+  async getCompartirArticulos() {
+    await Share.share({
+      title: this.articulo.title,
+      text: this.articulo.source.name,
+      url: this.articulo.url,
+      dialogTitle: 'Compartir con redes sociales',
+    });
+  }
+
+  getToggleFavoritos() {
+    console.log('gettoggleFavoritos');
   }
 }
